722d4e27b55da04e14c77a2becb1df5c54290b7d
deena <deenapoulose123@mail.com>
2021-06-16 14:47:15 +0530

    last

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,33 +1,3 @@
-// import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
-// import thunk from 'redux-thunk';
-// import { userSigninReducer,} from './reducers/userReducers';
-// import {
-//   productDetailsReducer,
-//   productListReducer,
-//   productDeleteReducer,
-// } from './reducers/productReducers';
-// const initialState = {
-//   userSignin: {
-//     userInfo: localStorage.getItem('userInfo')
-//       ? JSON.parse(localStorage.getItem('userInfo'))
-//       : null,
-//   },
-  
-// };
-// const reducer = combineReducers({
-//   userSignin: userSigninReducer,
-//   productList: productListReducer,
-//   productDetails: productDetailsReducer,
-//   productDelete: productDeleteReducer,
-// });
-// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(
-//   reducer,
-//   initialState,
-//   composeEnhancer(applyMiddleware(thunk))
-// );
-
-// export default store;
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
